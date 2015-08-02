451dc21d3c4ecfc13df9690bd94c7050dad18369
Andrés Luna <andresluna2007@gmail.com>
2015-08-02 20:34:47 +0200

    renamed Main.java to PJeniDemoApp.java

---
+++ jeni/src/synalp/generation/probabilistic/guidemo/PJeniDemoApp.java
@@ -0,0 +1,30 @@
+package synalp.generation.probabilistic.guidemo;
+
+import java.awt.EventQueue;
+
+public class PJeniDemoApp
+{
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					AppWindow frame = new AppWindow();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+}
